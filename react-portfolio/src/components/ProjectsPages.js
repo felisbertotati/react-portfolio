0938f37fb182c7c7c73bd0b5188c42a79ebef73d
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-03-08 16:27:34 +0000

    - added cards in skills

--- react-portfolio/src/components/ProjectsPages.js
+++ react-portfolio/src/components/ProjectsPages.js
@@ -1,7 +1,27 @@
 import React from "react";
+import myWork from "../myprojects.json";
 
 function ProjectsPages() {
-  return <h1>ProjectsPages</h1>;
+  return (
+    <div className="container">
+      <div className="row">
+        {myWork.map((work) => (
+          <div className="col-md-4" key={work.id}>
+            <div className="card">
+              <div className="card-body">
+                <h3 className="card-text">{work.name}</h3>
+                {/* <img className="card-img">{work.image}</img> */}
+                <p className="card-text">{work.about}</p>
+                <a href="/" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsPages;
