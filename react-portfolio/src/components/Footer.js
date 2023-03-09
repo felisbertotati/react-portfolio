c9e647c3e3df8e2af2fd049d6e7875f210c0a6fe
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-03-09 22:18:54 +0000

    -added icons -style icons -style contcats

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function footer() {
   return (
     <>
-      <div className="footer">
-        <footer>
-          <p className="rights">© 2023 Tatiana Leitao. All Rights Reserved.</p>
-        </footer>
-      </div>
+      <footer className="footer">
+        <div className="social-icons">
+          <a
+            href="https://github.com/felisbertotati"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tatiana-leitao-7b07b51a1/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://twitter.com/tatianaleitao21"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitter />
+          </a>
+        </div>
+        <p className="rights">© 2023 Tatiana Leitao. All Rights Reserved.</p>
+      </footer>
     </>
   );
 }
--- react-portfolio/src/pages/Contacts.js
+++ react-portfolio/src/pages/Contacts.js
@@ -3,38 +3,43 @@ import React from "react";
 function Contacts() {
   return (
     <>
-      <h2 className="skills">Contact me</h2>
-      <div className="container">
-        <div class="input-group pt-5">
-          <div class="input-group-prepend"></div>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Ricky"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </div>
-        <div class="input-group pt-3">
-          <div class="input-group-prepend"></div>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Ricky@gmail.com"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </div>
-        <div class="input-group pt-3">
-          <div class="input-group-prepend"></div>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Message"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            style={{ height: 150 }}
-          />
+      <div className="wrapper">
+        <h2 className="skills">Contact me</h2>
+        <div className="container">
+          <div class="input-group pt-3">
+            <div class="input-group-prepend"></div>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Ricky"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div class="input-group pt-3">
+            <div class="input-group-prepend"></div>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Ricky@gmail.com"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div class="input-group pt-3">
+            <div class="input-group-prepend"></div>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Message"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              style={{ height: 150 }}
+            />
+          </div>
+          <a href="/" className="btn contacts-button">
+            Send
+          </a>
         </div>
       </div>
     </>
